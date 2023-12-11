f462be137144019a8834bee0da84cceeed770194
hamzahdiza <hamzah.diza.santoso@bni.co.id>
2023-12-11 11:52:17 +0700

    android test

--- src/app/(core)/breakdownCategory/page.tsx
+++ src/app/(core)/breakdownCategory/page.tsx
@@ -25,6 +25,7 @@ export default function BreakdownCategory() {
   };
 
   useEffect(() => {
+    let AndroidTest: any;
     if (AndroidTest) {
       const token = AndroidTest.getToken();
       setToken(token);
